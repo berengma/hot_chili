a09521f44c674900decffdca510fe36c942ed558
berengma <Gundul@mailbox.org>
2021-01-25 13:08:43 +0100

    add get_use_to

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
  
-This mod makes your avator burn when you eat hot and spicy food :D
+This mod makes your avatar burn when you eat hot and spicy food :D
 
 
 register your own food with:
 
-hot_chili.register_hot_food(name,burntime)
+hot_chili.register_hot_food(name,burntime,get_used_to_after)
 
-
-
-
+name 			is the string of the foodname
+burntime 			will be the seconds you were burning (default 10)
+get_use_to_after	will be the number of uses until you got used to that food (default always)
+				(negative number means you will never get used to)
+				
 
 (c)2021 Copyright by Gundul - WTFPL License   -- Eat what the fuck you want to XD
 
--- init.lua
+++ init.lua
@@ -9,23 +9,30 @@ hot_chili.food = {}
 minetest.register_on_item_eat(function(hp_change, replace_with_item, itemstack, user, pointed_thing)
                               
 	if not user or not user:is_player() then return end
-	
+                             
+	local meta = user:get_meta()
+	local eaten = meta:get_int("hot_chili_spicy")
 	local name = itemstack:get_name()
 	local burntime = hot_chili.food[name]
-	if burntime then
-		fire_plus.burn_player(user, burntime, 0)
+	if burntime and burntime.time and (eaten < burntime.usedto or burntime.usedto < 0) then
+		fire_plus.burn_player(user, burntime.time, 0)
+		eaten = eaten +1
+		meta:set_int("hot_chili_spicy",eaten)
 	end
 end)
 
 
 
 
-function hot_chili.register_hot_food(name,time)
+function hot_chili.register_hot_food(name,time,usedto)
 	
+	if not usedto then usedto = -1 end
 	if not time then time = 10 end
 	if not name or hot_chili.food[name] then return false end
+	hot_chili.food[name] = {}
 	
-	hot_chili.food[name] = time
+	hot_chili.food[name].time = time
+	hot_chili.food[name].usedto = usedto
 	
 	return true
 end
@@ -33,7 +40,7 @@ end
 
 -- registration of hot food
 
-hot_chili.register_hot_food("farming:chili_pepper",10)
-hot_chili.register_hot_food("farming:chili_bowl",20)
-hot_chili.register_hot_food("farming:chili_bibimbap",20)
+hot_chili.register_hot_food("farming:chili_pepper",10,10)
+hot_chili.register_hot_food("farming:chili_bowl",20,5)
+hot_chili.register_hot_food("farming:chili_bibimbap",20,5)
 
